c1f97dd11544fd64d3688bd47005e061a10e9272
Manav Desai <manavdesai30399@gmail.com>
2020-08-28 20:53:34 +0530

    added image helper

--- instafrontend/src/components/Header/Header.js
+++ instafrontend/src/components/Header/Header.js
@@ -2,8 +2,14 @@ import React from "react";
 import "./Header.css";
 import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
 import FavoriteBorderRoundedIcon from "@material-ui/icons/FavoriteBorderRounded";
+import NearMeOutlinedIcon from "@material-ui/icons/NearMeOutlined";
+import ExploreOutlinedIcon from "@material-ui/icons/ExploreOutlined";
+import ImageHelper from "../ImageHelper/ImageHelper";
+import { isAuthenticated } from "../Authentication/helper/authenticationHelper";
 
 const Header = () => {
+  const { user, token } = isAuthenticated();
+
   return (
     <div className="header__container">
       <div className="header__empty"></div>
@@ -19,8 +25,9 @@ const Header = () => {
         />
       </div>
       <div className="header__right">
-        <HomeRoundedIcon />
-        <FavoriteBorderRoundedIcon />
+        <HomeRoundedIcon style={{ border: "1px black" }} />
+        <FavoriteBorderRoundedIcon tyle={{ border: "1px" }} />
+        <ImageHelper id={user._id} size="small" />
       </div>
       <div className="header__empty"></div>
     </div>
--- instafrontend/src/components/ImageHelper/ImageHelper.js
+++ instafrontend/src/components/ImageHelper/ImageHelper.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { API } from "../../backend";
+import { makeStyles } from "@material-ui/core/styles";
+import { Avatar } from "@material-ui/core";
 
-const ImageHelper = () => {
-  return <div></div>;
+const ImageHelper = ({ id, size }) => {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+    small: {
+      width: theme.spacing(3.5),
+      height: theme.spacing(3.5),
+      marginLeft: "17px",
+      marginTop: "5px",
+    },
+    large: {
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+    },
+  }));
+
+  const classes = useStyles();
+
+  const image = () => {
+    if (size === "small") {
+      return <Avatar src={imageURL} className={classes.small} />;
+    } else {
+      return <Avatar src={imageURL} className={classes.large} />;
+    }
+  };
+
+  const imageURL = id
+    ? `${API}/profile/photo/${id}`
+    : "https://images.pexels.com/photos/3561339/pexels-photo-3561339.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+
+  return <div>{image()}</div>;
 };
 
 export default ImageHelper;
